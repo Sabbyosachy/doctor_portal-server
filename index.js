e16059857268482061d7768667a4fe2211894f33
Sabbyosachy <subbyosachychakraborty@gmail.com>
2021-11-04 09:47:10 +0600

    second

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.edakp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+async function run(){
+ try{
+    await client.connect();
+    console.log('connected successfully');
+
+ }
+finally{
+    // await client.close();
+}
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
